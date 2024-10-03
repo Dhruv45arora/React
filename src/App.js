5ebe0af4e0bdb2fc86aabd04cb1695298f3c494d
Dhruv arora <dhruv.ispecia@gmail.com>
2024-10-03 17:36:39 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Heading from './components/header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const Heading=()=>{
+    return(
+        <div>
+            <h1>
+                This is our first react component
+            </h1>
+            
+            </div>
+    )
+}
+export default Heading
